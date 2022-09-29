8d1c529e20691b6c1ac748735adb4c41681fc973
Anthony Guzzardo <113217959+aguzzar2@users.noreply.github.com>
2022-09-28 20:14:49 -0500

    Commit 1

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -5,8 +5,8 @@ public class TestApp {
 
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
-
-        //TODO Print your name like this "Name: Vahid Alizadeh"
+        System.out.println("Name: Anthony Guzzardo");
+        
 
     }
 }
