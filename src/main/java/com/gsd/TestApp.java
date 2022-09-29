dd96684288eba0193af40cb2019cbb645bc4d690
Sam Grossman <samuel.w.grossman.com>
2022-09-28 20:05:52 -0500

    Removed newline

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -4,9 +4,9 @@ public class TestApp {
 
     public static void main(String[] args) {
         System.out.println("SE475/371 GSD Course \n");
-        System.out.println("Name: Sam Grossman \n");
 
         // TODO Print your name like this "Name: Vahid Alizadeh"
+        System.out.println("Name: Sam Grossman");
 
     }
 }
