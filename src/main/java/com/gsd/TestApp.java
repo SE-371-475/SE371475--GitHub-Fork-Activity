86f598e04fd783418824eaf9d5cc87d57e588d25
Chawaphong <ckhunsoo@depaul.edu>
2022-09-28 19:58:58 -0500

    Chawaphong : my added feature

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -4,7 +4,7 @@ public class TestApp {
 
 
     public static void main(String[] args){
-        System.out.println("SE475/371 GSD Course \n");
+        System.out.println("Name : Chawaphong Khunsoongnoen \n");
 
         //TODO Print your name like this "Name: Vahid Alizadeh"
 
