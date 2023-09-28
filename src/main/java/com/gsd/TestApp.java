0a63d786dddff3d0a0297a9bb8625d69fd040c71
Kashev Jaswal <kjaswal@depaul.edu>
2023-09-27 20:49:30 -0500

    Added comment in file

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -5,7 +5,7 @@ public class TestApp {
 
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
-
+        System.out.println("Name: Kashev Jaswal");
         //TODO Print your name like this "Name: Vahid Alizadeh"
 
     }
