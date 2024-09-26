3a81b5b83dc1efa14b8143d7671826c11a754717
Caleb Willingham <144171549+6Namez@users.noreply.github.com>
2024-09-25 19:41:08 -0500

    Update TestApp.java

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -5,8 +5,9 @@ public class TestApp {
 
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
+        System.out.println("Name: Caleb Willingham");
 
-        //TODO Print your name like this "Name: Vahid Alizadeh"
+
 
     }
 }
