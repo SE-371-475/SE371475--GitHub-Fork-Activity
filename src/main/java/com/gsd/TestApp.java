6289146f82664313dd34077e58d6a1ae7d29ba47
novakjared <jnovak31@depaul.edu>
2025-09-24 20:52:03 -0500

    Add the other names back then add Jared Novak

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -5,7 +5,16 @@ public class TestApp {
 
     public static void main(String[] args){
         System.out.println("SE475/371 GSD Course \n");
-        System.out.println("Jared Novak");
+        System.out.println("Janiya Garner");
 
+
+        //TODO Print your name like this "Name: Vahid Alizadeh"
+        System.out.println("Name: Viviana Flores");
+
+        System.out.println("Name: Ali Azfar");
+
+        System.out.println("Name: Ray Bartlett");
+
+        System.out.println("Name: Jared Novak");
     }
 }
